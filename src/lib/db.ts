bdd8718932af1790ae628b6c96695f21b95ef80f
John Andrew Moquia <andrewmoquia09@gmail.com>
2025-12-23 01:38:31 +0800

    fix: serverless issue with vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,16 @@
 import postgres from 'postgres';
 
+declare global {
+    var sql: ReturnType<typeof postgres> | undefined;
+}
+
 const connectionString = process.env.DATABASE_URL!;
 
-const sql = postgres(connectionString, {
-    ssl: 'require',
+// Use nullish coalescing assignment
+const sql = (globalThis.sql ??= postgres(connectionString, {
+    ssl: { rejectUnauthorized: false },
     max: 10,
     idle_timeout: 20,
-});
+}));
 
 export default sql;
